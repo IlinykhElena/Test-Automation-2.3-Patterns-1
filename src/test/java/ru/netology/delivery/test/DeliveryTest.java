ad2fe154357299d46949ff649db403e510968baf
ElenaIlinykh <bengaliki22@gmail.com>
2022-10-29 22:25:55 +0700

    test corrected

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -32,17 +32,20 @@ public class DeliveryTest {
         $("[data-test-id=\"phone\"] .input__control").setValue(validUser.getPhone());
         $("[data-test-id=\"agreement\"] .checkbox__box").click();
         $(byText("Запланировать")).click();
-        $("[data-test-id=\"success-notification\"] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + date1), Duration.ofSeconds(15));
-        $(".notification__content").shouldBe(visible);
 
-        $("[data-test-id=\"success-notification\"] .notification__closer").click();
+        $("div[data-test-id='success-notification']").should(Condition.visible, Duration.ofSeconds(15));
+        String notification__content = "Встреча успешно запланирована на ";
+        $("div.notification__content").shouldHave(Condition.text(notification__content + date1));
+
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[placeholder=\"Дата встречи\"]").setValue(date2);
-        $("[data-test-id=\"agreement\"] .checkbox__box").click();
         $(byText("Запланировать")).click();
-        $("[data-test-id=\"replan-notification\"]").should(visible, Duration.ofSeconds(15));
-        $(byText("Перепланировать")).click();
-        $("[data-test-id=\"success-notification\"]").should(visible, Duration.ofSeconds(15));
-        $("[data-test-id=\"success-notification\"]").shouldHave(Condition.text("Встреча успешно забронирована на " + date2), Duration.ofSeconds(15));
+        $("div[data-test-id='replan-notification']").should(Condition.visible);
+        String replan__button__text = "Перепланировать";
+        $("div[data-test-id='replan-notification'] .button__text").shouldHave(Condition.text(replan__button__text));
+        $("div[data-test-id='replan-notification'] .button__text").click();
+
+        $("div[data-test-id='success-notification']").should(Condition.visible, Duration.ofSeconds(15));
+        $("div.notification__content").shouldHave(Condition.text(notification__content + date2));
     }
 }
